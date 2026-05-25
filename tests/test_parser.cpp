35822db6d28d95c1b2556b89d78986979772486f
Iliya Ivanov <ilmaivanov@edu.hse.ru>
2026-05-25 14:45:18 +0300

    new tests

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,32 +1,139 @@
 #include <catch2/catch_test_macros.hpp>
 #include "parser.h"
 
-TEST_CASE("Parser basic input output filter parsing")
+static char* cstr(const char* s) { return const_cast<char*>(s); }
+
+TEST_CASE("empty args")
 {
-    ArgsParser parser;
+    ArgsParser p;
+    char* argv[] = { cstr("prog") };
+
+    REQUIRE(p.parse(1, argv) != Result::ok);
+}
+
+TEST_CASE("basic input output")
+{
+    ArgsParser p;
 
     char* argv[] = {
-        (char*)"prog",
-        (char*)"-i",
-        (char*)"input.txt",
-        (char*)"-o",
-        (char*)"output.txt",
-        (char*)"-f",
-        (char*)"blur",
-        (char*)"5",
-        (char*)"10"
+        cstr("prog"),
+        cstr("-i"), cstr("in.txt"),
+        cstr("-o"), cstr("out.txt")
     };
 
-    auto res = parser.parse(9, argv);
+    REQUIRE(p.parse(5, argv) == Result::ok);
+    REQUIRE(p.getInFileName() == "in.txt");
+    REQUIRE(p.getOutFileName() == "out.txt");
+}
 
-    REQUIRE(res == Result::ok);
-    REQUIRE(parser.getInFileName() == "input.txt");
-    REQUIRE(parser.getOutFileName() == "output.txt");
+TEST_CASE("input given twice should fail")
+{
+    ArgsParser p;
 
-    auto filters = parser.getFilters();
+    char* argv[] = {
+        cstr("prog"),
+        cstr("-i"), cstr("a.txt"),
+        cstr("-i"), cstr("b.txt")
+    };
+
+    REQUIRE(p.parse(5, argv) == Result::badArgs);
+}
+
+TEST_CASE("output given twice should fail")
+{
+    ArgsParser p;
+
+    char* argv[] = {
+        cstr("prog"),
+        cstr("-o"), cstr("a.txt"),
+        cstr("-o"), cstr("b.txt")
+    };
+
+    REQUIRE(p.parse(5, argv) == Result::badArgs);
+}
+
+TEST_CASE("unknown argument should fail")
+{
+    ArgsParser p;
+
+    char* argv[] = {
+        cstr("prog"),
+        cstr("random.txt")
+    };
+
+    REQUIRE(p.parse(2, argv) == Result::badArgs);
+}
+
+TEST_CASE("filter with name and params")
+{
+    ArgsParser p;
+
+    char* argv[] = {
+        cstr("prog"),
+        cstr("-f"),
+        cstr("blur"),
+        cstr("5"),
+        cstr("10")
+    };
+
+    REQUIRE(p.parse(5, argv) == Result::ok);
+
+    auto filters = p.getFilters();
     REQUIRE(filters.size() == 1);
     REQUIRE(filters[0].filterName == "blur");
     REQUIRE(filters[0].params.size() == 2);
     REQUIRE(filters[0].params[0] == "5");
     REQUIRE(filters[0].params[1] == "10");
+}
+
+TEST_CASE("filter without name should fail")
+{
+    ArgsParser p;
+
+    char* argv[] = {
+        cstr("prog"),
+        cstr("-f"),
+        cstr("5")
+    };
+
+    REQUIRE(p.parse(3, argv) == Result::badArgs);
+}
+
+TEST_CASE("mix input output filter full scenario")
+{
+    ArgsParser p;
+
+    char* argv[] = {
+        cstr("prog"),
+        cstr("-i"), cstr("in.txt"),
+        cstr("-o"), cstr("out.txt"),
+        cstr("-f"), cstr("blur"), cstr("5"), cstr("10"),
+        cstr("-f"), cstr("sharpen"), cstr("2")
+    };
+
+    REQUIRE(p.parse(10, argv) == Result::ok);
+
+    REQUIRE(p.getInFileName() == "in.txt");
+    REQUIRE(p.getOutFileName() == "out.txt");
+
+    auto filters = p.getFilters();
+    REQUIRE(filters.size() == 2);
+
+    REQUIRE(filters[0].filterName == "blur");
+    REQUIRE(filters[0].params.size() == 2);
+
+    REQUIRE(filters[1].filterName == "sharpen");
+    REQUIRE(filters[1].params.size() == 1);
+}
+
+TEST_CASE("flag without value should fail")
+{
+    ArgsParser p;
+
+    char* argv[] = {
+        cstr("prog"),
+        cstr("-i")
+    };
+
+    REQUIRE(p.parse(2, argv) == Result::badArgs);
 }
