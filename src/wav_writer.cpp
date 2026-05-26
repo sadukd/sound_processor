4eec40370e31a7f05ae6833c1960c76e00b352d9
Iliya Ivanov <ilmaivanov@edu.hse.ru>
2026-05-26 16:44:53 +0300

    writer

--- src/wav_writer.cpp
+++ src/wav_writer.cpp
@@ -1,6 +1,40 @@
 #include "wav_writer.h"
+#include "wav_structures.h"
 
 void writer(const std::string& path, const Waveform& wf)
 {
-    size_t fileSize = 36 + wf.samplesCount() * 
+    uint32_t fileSize = 36 + wf.samplesCount() * wf.SampleSize / 8;
+
+    RiffHeader riff{
+        {'R', 'I', 'F', 'F'},
+        fileSize,
+        {'W', 'A', 'V', 'E'}
+    };
+
+    ChunkHeader fmtHeader{
+        {'f', 'm', 't', ' '},
+        sizeof(FmtPayload)
+    };
+
+    FmtPayload fmtPayload{
+        wf.AudioFormat,
+        wf.ChannelAmnt,
+
+        wf.SampleRate,
+        wf.SampleRate * wf.ChannelAmnt *
+        wf.SampleSize / 8,
+
+        static_cast<uint16_t>(
+            wf.ChannelAmnt *
+            wf.SampleSize / 8
+        ),
+        wf.SampleSize
+    };
+
+    ChunkHeader dataHeader{
+        {'d', 'a', 't', 'a'},
+        wf.samplesCount() * wf.SampleSize / 8
+    };
+
+    
 }
