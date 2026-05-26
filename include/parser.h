2bcaaba97d167029f40e34322c4a20c62e199a75
Iliya Ivanov <ilmaivanov@edu.hse.ru>
2026-05-26 13:55:44 +0300

    clang format

--- include/parser.h
+++ include/parser.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <vector>
 #include <string>
+#include <vector>
 
 struct FilterDescriptor
 {
@@ -9,22 +9,28 @@ struct FilterDescriptor
     std::vector<std::string> params;
 };
 
-enum class Result {
+enum class Result
+{
     ok,
-    noArgs,		// show help
-    badArgs,	// incorrect arguments sequence
+    noArgs,   // show help
+    badArgs,  // incorrect arguments sequence
 };
 
-class ArgsParser {
+class ArgsParser
+{
 public:
     Result parse(int argc, char* argv[]);
+
 public:
     const std::string& getInFileName() const { return _inFileName; }
     const std::string& getOutFileName() const { return _outFileName; }
-    const std::vector<FilterDescriptor>& getFilters() const { return _filterDescriptors; }
+    const std::vector<FilterDescriptor>& getFilters() const
+    {
+        return _filterDescriptors;
+    }
 
 private:
     std::string _inFileName;
     std::string _outFileName;
-    std::vector<FilterDescriptor> _filterDescriptors;    
+    std::vector<FilterDescriptor> _filterDescriptors;
 };
